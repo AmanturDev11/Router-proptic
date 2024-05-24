85a71f80cf2841c9f93d0d9d272f930fe7944e5c
AmanturDev11 <ismailovamantur22@gmail.com>
2024-05-25 00:10:38 +0600

    router-proptic

--- src/components/pages/students/StudentDetails.jsx
+++ src/components/pages/students/StudentDetails.jsx
@@ -21,19 +21,45 @@ const StudentDetails = () => {
 	if (!student) return <div>Loading...</div>;
 
 	return (
-		<Container>
-			<h1>{student.name}</h1>
-			<img src={student.image} alt={student.name} />
-			<p>Age: {student.age}</p>
-		</Container>
+		<StudentDetailsContent>
+			<div className="container">
+				<Content>
+					<Cards>
+						<h1>{student.name}</h1>
+						<img src={student.image} alt={student.name} />
+						<p>Age: {student.age}</p>
+					</Cards>
+				</Content>
+			</div>
+		</StudentDetailsContent>
 	);
 };
 
 export default StudentDetails;
 
-const Container = styled.div`
+const StudentDetailsContent = styled.div`
 	display: flex;
-	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Content = styled.div`
+	display: flex;
 	align-items: center;
 	justify-content: center;
+	flex-direction: column;
+`;
+
+const Cards = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	border: 1px solid;
+	width: "470px";
+	height: "390px";
+	border-radius: "6px";
+	img {
+		width: 300px;
+	}
 `;
